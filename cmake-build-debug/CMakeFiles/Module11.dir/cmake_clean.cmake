1bed1ccd1810ee5b76a679a16a2de8c2f5c91e26
augoodma <augoodma@asu.edu>
2020-10-01 09:34:52 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/Module11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module11.dir/Simulator.c.o"
+  "Module11"
+  "Module11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
